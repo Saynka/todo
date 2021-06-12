907fa9d33d5f9ccac4ead761fdb41f40a9f0d923
Saynka <sankakale@gmail.com>
2021-06-12 15:17:51 -0700

    lab31

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,31 +2,52 @@ import { useState, useContext } from 'react';
 import { SiteContext } from '../context/Site.js';
 import { ThemeContext } from '../context/Theme.js';
 
-function Form() {
+function Form(props) {
 
-  const [newTitle, setNewTitle] = useState('');
+  const [item, setItem] = useState({});
 
   // this "opts into" the context, "grabbing it" from the blob
-  const siteInformation = useContext(SiteContext);
-  const themeStuff = useContext(ThemeContext);
+  // const siteInformation = useContext(SiteContext);
+  // const themeStuff = useContext(ThemeContext);
 
-  const handleChange = (e) => {
-    // what they typed is: e.target.value
-    setNewTitle(e.target.value);
-  }
+  const handleChange = e => {
+    setItem({ item: { ...item, [e.target.name]: e.target.value } });
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    siteInformation.changeTitleTo(newTitle);
-  }
+    e.target.reset();
+    props.handleSubmit(item);
+    setItem({});
+  };
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   siteInformation.changeTitleTo(newTitle);
+  // }
 
   return (
     <>
+      <h3>Add Item</h3>
       <form onSubmit={handleSubmit}>
-        <input name="title" placeholder="New Title?" onChange={handleChange} />
+        <label>
+          <span>To Do Item</span>
+          <input
+            name="text"
+            placeholder="Add To Do List Item"
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          <span>Difficulty Rating</span>
+          <input defaultValue="1" type="range" min="1" max="5" name="difficulty" onChange={handleChange} />
+        </label>
+        <label>
+          <span>Assigned To</span>
+          <input type="text" name="assignee" placeholder="Assigned To" onChange={handleChange} />
+        </label>
+        <button>Add Item</button>
       </form>
-      <hr />
-      <button onClick={themeStuff.toggleMode}>Change Theme</button>
     </>
   )
 
